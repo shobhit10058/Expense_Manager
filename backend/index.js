7f4a34b5a3224886e21475c43c3cc52ec2c8b99f
shobhi10058 <rkgupta462gkp@gmail.com>
2022-12-13 22:18:38 +0530

    routers added on index

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const userRouter = require("./routes/user");
+const expenseRouter = require("./routes/expense");
 
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 
 // routes
 app.use('/users', userRouter);
+app.use('/expense', expenseRouter);
 
 // connect to db
 mongoose.connect(process.env.DB_URI, { 
